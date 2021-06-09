882b99d4a7ecd4cb7ba253f38748e37f9893bc0b
Preston Kemp <preston.kemp.1@us.af.mil>
2021-06-09 17:55:14 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Preston Kemp on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
