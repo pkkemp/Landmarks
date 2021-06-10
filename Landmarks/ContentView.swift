b8637d97600827e58245054e2b9b7b2be49b14a7
Preston Kemp <preston.kemp.1@us.af.mil>
2021-06-09 23:05:00 -0500

    finished creating and combining views

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .ignoresSafeArea()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Turtle Rock")
+                        .font(.title)
+                }
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Description Goes Here")
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
 
